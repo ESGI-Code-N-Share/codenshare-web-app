e1edce81cf50c2a7f0af94697e94dba7e034491c
corentin lechêne <corentin.lechene.pro@gmail.com>
2024-07-06 11:53:01 +0200

    add preview

--- src/components/files/OutputFile.vue
+++ src/components/files/OutputFile.vue
@@ -3,25 +3,15 @@
 
 const url = defineModel('url', {type: String as () => string | null, required: false})
 
-const downloadFile = () => {
-  // todo download file
-}
-
 </script>
 
 <template>
   <div
-      class="drop-zone border-gray-500 border-round p-3 text-center text-color-secondary h-auto"
+      class="drop-zone border-gray-500 border-round p-2 text-center text-color-secondary h-auto"
       style="background-color: var(--gray-800);"
   >
-    <img
-        v-if="url"
-        :class="`max-h-15rem`"
-        :src="url"
-        alt="image"
-        class="w-full h-full cursor-pointer"
-        style="object-fit: contain"/>
-    <div v-else class="flex flex-column gap-1 justify-content-center align-items-center">
+    <Image v-if="url" :src="url" alt="Image" preview width="250"/>
+    <div v-else class="flex flex-column gap-1 p-2 justify-content-center align-items-center">
       <i class="pi pi-image text-6xl"/>
       <small class="text-color-secondary">{{ $t('program.output_files.title') }}</small>
     </div>
