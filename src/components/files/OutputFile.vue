13a075fc2f60c1bce60504732842716a163971e8
corentin lechêne <corentin.lechene.pro@gmail.com>
2024-07-20 18:06:21 +0200

    add output file other than image

--- src/components/files/OutputFile.vue
+++ src/components/files/OutputFile.vue
@@ -2,7 +2,8 @@
 
 
 const url = defineModel('url', {type: String as () => string | null, required: false})
-defineEmits(['onError'])
+defineProps<{ type: string }>()
+defineEmits(['onError', 'onDownload'])
 
 </script>
 
@@ -11,7 +12,11 @@ defineEmits(['onError'])
       class="drop-zone border-gray-500 border-round p-2 text-center text-color-secondary h-auto"
       style="background-color: var(--gray-800);"
   >
-    <Image v-if="url" :src="url" alt="Image" preview width="250" @error="$emit('onError')"/>
+    <div v-if="type === 'text/plain' && url" class="flex flex-column gap-2 p-3">
+      <i class="pi pi-upload text-6xl cursor-pointer" @click="$emit('onDownload')"/>
+      <small class="text-color-secondary">{{ $t('program.output_files.download') }}</small>
+    </div>
+    <Image v-else-if="url" :src="url" alt="Image" preview width="250" @error="$emit('onError')"/>
     <div v-else class="flex flex-column gap-1 p-2 justify-content-center align-items-center">
       <i class="pi pi-image text-6xl"/>
       <small class="text-color-secondary">{{ $t('program.output_files.title') }}</small>
